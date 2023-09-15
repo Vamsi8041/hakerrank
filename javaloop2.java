c2e520fa1a09bb7cefb400d87ea0d417e9c95796
Bankupalli Ramamohana Vamsi <133898188+Vamsi8041@users.noreply.github.com>
2023-09-15 23:00:33 +0530

    Add files via upload

---
+++ javaloop2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class javaloop2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum=a;
+            int min=0;
+            for(int j=0;j<n;j++){
+                min+=Math.pow(2,j)*b;
+                sum+=min;
+                min=0;
+                
+            System.out.print(sum+" ");
+            }
+        System.out.println();
+        }
+        in.close();
+        
+        
+    }
+}
